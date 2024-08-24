d24d0f98a6f89e8c0e4adfbd5f0af67b74f2bda9
Su5eD <su5ed@outlook.com>
2024-08-24 11:13:37 +0200

    Fabric

--- common/src/main/java/com/example/examplemod/CommonClass.java
+++ common/src/main/java/com/example/examplemod/CommonClass.java
@@ -1,9 +1,10 @@
 package com.example.examplemod;
 
 import com.example.examplemod.platform.Services;
-import com.glisco.isometricrenders.util.ImageIO;
 import com.glisco.isometricrenders.render.ItemRenderable;
 import com.glisco.isometricrenders.render.RenderableDispatcher;
+import com.glisco.isometricrenders.util.ImageIO;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
@@ -28,45 +29,62 @@ public class CommonClass {
 
     public static void runRender() {
         if (shouldRender()) {
-            renderItems().join();
+            List<Pair<ResourceLocation, Item>> renderable = getRenderableItems();
+            if (!renderable.isEmpty()) {
+
+//                Minecraft.getInstance().setScreen(new RenderingScreen(Component.literal("Rendering Items"), counter, renderable.size()));
+
+                Constants.LOG.info("Rendering {} items", renderable.size());
+                renderItems(renderable).join();
+            }
+
             Constants.LOG.info("Render complete, shutting down");
             Minecraft.getInstance().stop();
         }
     }
 
-    private static boolean shouldRender() {
+    public static boolean shouldRender() {
         return System.getProperty(RENDER_PROPERTY) != null;
     }
 
-    private static CompletableFuture<?> renderItems() {
+    private static List<Pair<ResourceLocation, Item>> getRenderableItems() {
         Set<String> namespaces = Set.of(System.getProperty(RENDER_PROPERTY).split(","));
         if (namespaces.isEmpty()) {
-            return CompletableFuture.completedFuture(null);
+            return List.of();
         }
 
         Constants.LOG.info("Rendering items for namespaces {}", namespaces);
 
-        List<CompletableFuture<?>> list = new ArrayList<>();
+        List<Pair<ResourceLocation, Item>> list = new ArrayList<>();
         for (Map.Entry<ResourceKey<Item>, Item> entry : BuiltInRegistries.ITEM.entrySet()) {
             ResourceLocation name = entry.getKey().location();
             if (namespaces.contains(name.getNamespace())) {
-                list.add(renderItem(name, entry.getValue()));
+                list.add(Pair.of(name, entry.getValue()));
             }
         }
+
+        return list;
+    }
+
+    private static CompletableFuture<?> renderItems(List<Pair<ResourceLocation, Item>> renderable) {
+        List<CompletableFuture<?>> list = renderable.stream()
+            .<CompletableFuture<?>>map(p -> renderItem(p.getFirst(), p.getSecond()))
+            .toList();
 
         return CompletableFuture.allOf(list.toArray(CompletableFuture[]::new));
     }
 
     private static CompletableFuture<?> renderItem(ResourceLocation name, Item item) {
         return Minecraft.getInstance().submit(() -> {
-            ItemStack stack = new ItemStack(item);
-            ItemRenderable renderable = new ItemRenderable(stack);
+                ItemStack stack = new ItemStack(item);
+                ItemRenderable renderable = new ItemRenderable(stack);
 
-            boolean depth = !IGNORE_DEPTH.contains(item) && Minecraft.getInstance().getItemRenderer().getModel(stack, null, null, 0).isGui3d();
-            setupItem(renderable, depth);
+                boolean depth = !IGNORE_DEPTH.contains(item) && Minecraft.getInstance().getItemRenderer().getModel(stack, null, null, 0).isGui3d();
+                setupItem(renderable, depth);
 
-            return scheduleRender(renderable, name.getNamespace(), name.getPath());
-        }).thenCompose(Function.identity());
+                return scheduleRender(renderable, name.getNamespace(), name.getPath());
+            })
+            .thenCompose(Function.identity());
     }
 
     private static CompletableFuture<File> scheduleRender(ItemRenderable renderable, String namespace, String fileName) {
